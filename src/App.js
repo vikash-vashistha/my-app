866b2a72d2408e2d9aa648515cd395c7d1c37bbe
vikash-vashistha <vikashvashitha.dev@gmail.com>
2024-02-03 15:09:04 +0530

    2nd question is also done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import FlavanoidsTable from './components/FlavanoidsTable.jsx';
+import GammaTable from './components/GamaTable.jsx';
 
 function App() {
   return (
     <div className="App">
        <FlavanoidsTable/>
+       <GammaTable/>
     </div>
   );
 }
